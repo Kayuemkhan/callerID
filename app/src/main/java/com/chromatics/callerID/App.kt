1038da024ce7212080ac32d6df09302aba51e48f
abdulkayuem <abdulkayuem007@gmail.com>
2025-01-25 00:51:51 +0600

    init project

---
+++ app/src/main/java/com/chromatics/callerID/App.kt
@@ -0,0 +1,14 @@
+package com.chromatics.callerID
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application() {
+
+  override fun onCreate() {
+    super.onCreate()
+
+  }
+
+}
